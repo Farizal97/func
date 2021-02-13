8c5a03d48ad13ca3ad92f70e6b9231ab19d3e2a3
R-Technology <43551882+Farizal97@users.noreply.github.com>
2021-02-14 05:35:27 +0700

    go management

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import  "fmt"
+
+// import (
+// 	"fmt"
+// 	"func/management"
+// )
+
+
+
+
+func main() {
+	
+
+	// user:= management.User{1,"Zelda","Safitri","zelda@inindo.com",true}
+	// result := user.display()
+
+	// fmt.Println(result)
+	//  user2:= management.User{2,"Link","awakening","link@inindo.com",true}
+	//  fmt.Println(user2.display())
+
+	// users := []management.User{user,user2}
+
+	// group := management.Group{"Gamer",user,users,true}
+
+	// // displayGroup(group)
+	// group.DisplayGroup()
+
+}
+
+
+
+
+	//sentence := printMyResult("saya sedang")
+	//fmt.Println(sentence)
+	//result := add(10,20)
+	//fmt.Println(result)
+
+	// luas,keliling := calculate(10,2)
+	// fmt.Println(luas)
+	// fmt.Println(keliling)
+
+	
+	
+
+// func displayUser(user User) string {
+// 	return fmt.Sprintf("Name: %s %s,Email : %s",user.FirstName,user.LastName,user.Email)
+// 	//return result
+// }
+
+// func calculate(panjang int,lebar int)(int,int) {
+// 	luas := panjang * lebar 
+// 	keliling := 2 * (panjang + lebar )
+
+// 	return luas,keliling
+// }
+
+// func calculate(panjang int,lebar int)(luas int,keliling int) {
+// 	luas = panjang * lebar 
+// 	keliling = 2 * (panjang + lebar )
+
+// 	return 
+// }
+
+
+
+// func add(number int,numberTwo int) int{
+// 	result := number + numberTwo
+// 	return result
+// }
+
+// func printMyResult(sentence string) string {
+// 	newSentence := sentence + "belajar golang"
+// 	return newSentence
+// }
---
+++ management/entity.go
@@ -0,0 +1,36 @@
+// package management
+
+// import "fmt"
+
+// type User struct{
+// 	ID int
+// 	FirstName string
+// 	LastName string
+// 	Email string
+// 	IsActive bool
+
+// }
+
+// type Group struct {
+// 	Name string
+// 	Admin User
+// 	Users []User
+// 	isAvailable bool
+
+// }
+
+// func (user User) display() string {
+// 	return fmt.Sprintf("Name: %s %s,Email : %s",user.FirstName,user.LastName,user.Email)
+// }
+
+// func (group Group) DisplayGroup(){
+// 	fmt.Printf("Name: %s",group.Name)
+// 	fmt.Println("")
+// 	fmt.Printf("Member count : %d",len(group.Users))
+// 	fmt.Println("")
+
+// 	fmt.Println("Users name :")
+// 	for _,user := range group.Users{
+// 		fmt.Println(user.FirstName)
+// 	}
+// 
